2dd37a1a4ec57380db822e777ebde8e69c03cc55
hackblack226 <149976555+hackblack226@users.noreply.github.com>
2023-11-10 09:16:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import devoir
+import art
+import colorama
+
+print(colorama.Fore.RED)
+print(art.text2art("MP2-Cyber"))
+print(art.text2art("Authentification"))
+print(colorama.Fore.MAGENTA)
+devoir.enregistrer_utilisateur()
+devoir.Authentification()
